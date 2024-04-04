7c7c1469a67004ed2740d660ca5b3f21e2cb41b2
AlexWheelock <framedabyss8@gmail.com>
2024-04-03 21:00:48 -0600

    init

--- RentalForm.vb
+++ RentalForm.vb
@@ -1,6 +1,178 @@
-﻿Option Explicit On
+﻿'Alex Wheelock
+'RCET 0625
+'Car Rental
+'Spring 2024
+'https://github.com/AlexWheelock/CarRental
+
+Option Explicit On
 Option Strict On
 Option Compare Binary
 Public Class RentalForm
+    Private Sub NameTextBox_TextChanged(sender As Object, e As EventArgs) Handles NameTextBox.TextChanged
 
+    End Sub
+
+    Private Sub AddressTextBox_TextChanged(sender As Object, e As EventArgs) Handles AddressTextBox.TextChanged
+
+    End Sub
+
+    Private Sub CityTextBox_TextChanged(sender As Object, e As EventArgs) Handles CityTextBox.TextChanged
+
+    End Sub
+
+    Private Sub StateTextBox_TextChanged(sender As Object, e As EventArgs) Handles StateTextBox.TextChanged
+
+    End Sub
+
+    Private Sub ZipCodeTextBox_TextChanged(sender As Object, e As EventArgs) Handles ZipCodeTextBox.TextChanged
+
+    End Sub
+
+    Private Sub BeginOdometerTextBox_TextChanged(sender As Object, e As EventArgs) Handles BeginOdometerTextBox.TextChanged
+
+    End Sub
+
+    Private Sub EndOdometerTextBox_TextChanged(sender As Object, e As EventArgs) Handles EndOdometerTextBox.TextChanged
+
+    End Sub
+
+    Private Sub DaysTextBox_TextChanged(sender As Object, e As EventArgs) Handles DaysTextBox.TextChanged
+
+    End Sub
+
+    Private Sub FileToolStripMenuItem1_Click(sender As Object, e As EventArgs) Handles FileToolStripMenuItem1.Click
+
+    End Sub
+
+    Private Sub CalculateToolStripMenuItem_Click(sender As Object, e As EventArgs) Handles CalculateToolStripMenuItem.Click
+
+    End Sub
+
+    Private Sub ClearToolStripMenuItem1_Click(sender As Object, e As EventArgs) Handles ClearToolStripMenuItem1.Click
+
+    End Sub
+
+    Private Sub SummaryToolStripMenuItem1_Click(sender As Object, e As EventArgs) Handles SummaryToolStripMenuItem1.Click
+
+    End Sub
+
+    Private Sub ExitToolStripMenuItem1_Click(sender As Object, e As EventArgs) Handles ExitToolStripMenuItem1.Click
+
+    End Sub
+
+    Private Sub NameLabel_Click(sender As Object, e As EventArgs) Handles NameLabel.Click
+
+    End Sub
+
+    Private Sub AddressLabel_Click(sender As Object, e As EventArgs) Handles AddressLabel.Click
+
+    End Sub
+
+    Private Sub RentalForm_Load(sender As Object, e As EventArgs) Handles MyBase.Load
+
+    End Sub
+
+    Private Sub CityLabel_Click(sender As Object, e As EventArgs) Handles CityLabel.Click
+
+    End Sub
+
+    Private Sub State4_Click(sender As Object, e As EventArgs) Handles State4.Click
+
+    End Sub
+
+    Private Sub ZipCodeLabel_Click(sender As Object, e As EventArgs) Handles ZipCodeLabel.Click
+
+    End Sub
+
+    Private Sub BeginOdometerLabel_Click(sender As Object, e As EventArgs) Handles BeginOdometerLabel.Click
+
+    End Sub
+
+    Private Sub EndOdometerLabel_Click(sender As Object, e As EventArgs) Handles EndOdometerLabel.Click
+
+    End Sub
+
+    Private Sub DaysLabel_Click(sender As Object, e As EventArgs) Handles DaysLabel.Click
+
+    End Sub
+
+    Private Sub OdometerGroupbox_Enter(sender As Object, e As EventArgs) Handles OdometerGroupbox.Enter
+
+    End Sub
+
+    Private Sub MilesradioButton_CheckedChanged(sender As Object, e As EventArgs) Handles MilesradioButton.CheckedChanged
+
+    End Sub
+
+    Private Sub KilometersradioButton_CheckedChanged(sender As Object, e As EventArgs) Handles KilometersradioButton.CheckedChanged
+
+    End Sub
+
+    Private Sub MilesDrivenLabel_Click(sender As Object, e As EventArgs) Handles MilesDrivenLabel.Click
+
+    End Sub
+
+    Private Sub MileChargeLabel_Click(sender As Object, e As EventArgs) Handles MileChargeLabel.Click
+
+    End Sub
+
+    Private Sub DayChargeLabel_Click(sender As Object, e As EventArgs) Handles DayChargeLabel.Click
+
+    End Sub
+
+    Private Sub DiscountLabel_Click(sender As Object, e As EventArgs) Handles DiscountLabel.Click
+
+    End Sub
+
+    Private Sub YouOweLabel_Click(sender As Object, e As EventArgs) Handles YouOweLabel.Click
+
+    End Sub
+
+    Private Sub TotalMilesTextBox_TextChanged(sender As Object, e As EventArgs) Handles TotalMilesTextBox.TextChanged
+
+    End Sub
+
+    Private Sub MileageChargeTextBox_TextChanged(sender As Object, e As EventArgs) Handles MileageChargeTextBox.TextChanged
+
+    End Sub
+
+    Private Sub DayChargeTextBox_TextChanged(sender As Object, e As EventArgs) Handles DayChargeTextBox.TextChanged
+
+    End Sub
+
+    Private Sub TotalDiscountTextBox_TextChanged(sender As Object, e As EventArgs) Handles TotalDiscountTextBox.TextChanged
+
+    End Sub
+
+    Private Sub TotalChargeTextBox_TextChanged(sender As Object, e As EventArgs) Handles TotalChargeTextBox.TextChanged
+
+    End Sub
+
+    Private Sub DiscountGroupbox_Enter(sender As Object, e As EventArgs) Handles DiscountGroupbox.Enter
+
+    End Sub
+
+    Private Sub AAAcheckbox_CheckedChanged(sender As Object, e As EventArgs) Handles AAAcheckbox.CheckedChanged
+
+    End Sub
+
+    Private Sub Seniorcheckbox_CheckedChanged(sender As Object, e As EventArgs) Handles Seniorcheckbox.CheckedChanged
+
+    End Sub
+
+    Private Sub CalculateButton_Click(sender As Object, e As EventArgs) Handles CalculateButton.Click
+
+    End Sub
+
+    Private Sub ClearButton_Click(sender As Object, e As EventArgs) Handles ClearButton.Click
+
+    End Sub
+
+    Private Sub SummaryButton_Click(sender As Object, e As EventArgs) Handles SummaryButton.Click
+
+    End Sub
+
+    Private Sub ExitButton_Click(sender As Object, e As EventArgs) Handles ExitButton.Click
+
+    End Sub
 End Class
